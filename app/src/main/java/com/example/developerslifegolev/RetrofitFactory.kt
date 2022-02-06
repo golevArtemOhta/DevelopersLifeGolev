dbb0f368497149da75e8a441c935f4fca24dc110
golevArtemOhta <golevartem1995@gmail.com>
2022-02-06 08:42:49 +0300

    first version.

--- app/src/main/java/com/example/developerslifegolev/RetrofitFactory.kt
+++ app/src/main/java/com/example/developerslifegolev/RetrofitFactory.kt
@@ -4,9 +4,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitFactory {
-    fun new() = Retrofit.Builder()
+
+    fun new(apiRequest: Class<ApiRequest>) = Retrofit.Builder()
         .baseUrl("https://developerslife.ru")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-        .create(ApiRequest::class.java)
+        .create(apiRequest)
 }
